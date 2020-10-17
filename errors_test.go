67d6ef8081fc151160b6b63f2e306a89e351f1b6
linxGnu <linhduonggnu@gmail.com>
2020-10-17 16:03:36 +0900

    Update tests

--- errors_test.go
+++ errors_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestErrors(t *testing.T) {
-	if IsWsrepNotReady(nil) || IsWsrepNotReady(fmt.Errorf("ERRor ")) || IsWsrepNotReady(fmt.Errorf("ERRor 1047: asd")) {
+	if IsWsrepNotReady(nil) || IsWsrepNotReady(fmt.Errorf("ERRor ")) {
 		t.Fatal()
 	}
 	if !IsWsrepNotReady(fmt.Errorf("Error 1047:ab")) || !IsWsrepNotReady(fmt.Errorf("ERROR 1047:cd")) {
